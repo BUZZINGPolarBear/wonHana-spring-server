8ccd9a4acc703a49eef946a2c1df5dd624fd0984
Jeonjunhowi <jjh63360@kau.kr>
2022-09-28 23:29:34 +0900

    Initial setting commit

---
+++ src/main/java/com/wonhana/kau/wonhana/WonHanaApplication.java
@@ -0,0 +1,13 @@
+package com.wonhana.kau.wonhana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonHanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WonHanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wonhana/kau/wonhana/WonHanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonhana.kau.wonhana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonHanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
