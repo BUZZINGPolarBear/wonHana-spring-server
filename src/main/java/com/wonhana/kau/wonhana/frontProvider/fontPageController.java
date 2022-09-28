3c24bd786d5150f57558e91a951ec9512eb7c689
Jeonjunhowi <jjh63360@kau.kr>
2022-09-29 04:41:51 +0900

    add: sample html/css files

---
+++ src/main/java/com/wonhana/kau/wonhana/frontProvider/fontPageController.java
@@ -0,0 +1,15 @@
+package com.wonhana.kau.wonhana.frontProvider;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class fontPageController {
+
+    @RequestMapping("/")
+    public String homePage(){
+        return "main_1";
+    }
+
+}
