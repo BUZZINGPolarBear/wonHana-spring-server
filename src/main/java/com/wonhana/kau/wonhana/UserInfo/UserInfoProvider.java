adfb3f0fcd29f96a53d267d238036045d5ea9bb0
Jeonjunhowi <jjh63360@kau.kr>
2022-10-09 16:14:54 +0900

    fix: user info folder name

---
+++ src/main/java/com/wonhana/kau/wonhana/UserInfo/UserInfoProvider.java
@@ -0,0 +1,10 @@
+package com.wonhana.kau.wonhana.UserInfo;
+
+//import  com.wonhana.kau.wonhana.UserInfo.model.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserInfoProvider {
+
+
+}
