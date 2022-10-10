fe3360b2481fd0245f1f084bd65a41e2741f48f4
Jeonjunhowi <jjh63360@kau.kr>
2022-10-10 10:30:13 +0900

    fix: query & chore: console.logs

--- src/main/java/com/wonhana/kau/wonhana/repository/UserInfoRepository.java
+++ src/main/java/com/wonhana/kau/wonhana/repository/UserInfoRepository.java
@@ -1,10 +1,13 @@
 package com.wonhana.kau.wonhana.repository;
 
 import com.wonhana.kau.wonhana.dto.UserInfoDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -14,6 +17,12 @@ import java.util.Map;
 public class UserInfoRepository {
 
     private final NamedParameterJdbcTemplate template;
+    private static JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     public UserInfoRepository(DataSource dataSource) {
         this.template = new NamedParameterJdbcTemplate(dataSource);
@@ -44,33 +53,18 @@ public class UserInfoRepository {
 
 
     public List<UserInfoDto> findById(Long id) {
-        String sql = "select *, (car + accountBalance + stock + house) as asset," +
+        String sql =
+        "SELECT U.id,name, email, state, houseLoanInterest, carLent, carInsurance, dues, communicationCost, subscribeFee, coffee, food, snack, liquidNCigarette, necessityProduct, oil, car, accountBalance, stock, house, houseLoan, carLoan, flexibleIncome, fixedIncome,\n" +
+                "       (car + accountBalance + stock + house) as asset,\n" +
+                "       (houseLoan + carLoan) as liabilities,\n" +
+                "        (fixedIncome + flexibleIncome) as income,\n" +
+                "        (houseLoanInterest + carLent + carInsurance + Dues + communicationCost + subscribeFee) as fixedExpenditure,\n" +
+                "        (coffee + food + snack + liquidNCigarette + necessityProduct + oil + lifeStyle) as flexibleExpenditure,\n" +
+                "        (houseLoanInterest + carLent + carInsurance + Dues + communicationCost + subscribeFee + coffee + food + snack + liquidNCigarette + necessityProduct + oil + lifeStyle) as outcome\n" +
+        "FROM User AS U, UserAsset AS UA, UserDebt AS UD, UserIncome AS UI, FlexibleExpenditure AS FlexibleE, FixedExpenditure AS FixedE\n" +
+        "WHERE UA.userId = U.id AND UD.userId = U.id AND UI.userId = U.id AND FlexibleE.userId = U.id AND FixedE.userId = U.id AND U.id = :User.id";
 
-                "(houseLoan + carLoan) as liabilities," +
-
-                "(fixedIncome + flexibleIncome) as income," +
-
-                "(houseLoanInterest + carLent + carInsurance + Dues + " +
-                "communicationCost + subscribeFee) as fixedExpenditure," +
-
-                "(coffee + food + snack + liquidNCigarette + necessityProduct + " +
-                "oil + leisureCost + stockPurchase) as flexibleExpenditure," +
-
-                "(houseLoanInterest + carLent + carInsurance + Dues +" +
-                "communicationCost + subscribeFee + coffee + food + snack +" +
-                "liquidNCigarette + necessityProduct + oil + " +
-                "leisureCost + stockPurchase) as outcome " +
-
-                "from FixedExpenditure, FlexibleExpenditure, UserAsset, UserDebt, UserIncome " +
-
-                "where FixedExpenditure.id = :FixedExpenditure.id and " +
-                "FlexibleExpenditure.id = :FlexibleExpenditure.id and " +
-                "UserAsset.id = :UserAsset.id and " +
-                "UserIncome.id = :UserIncome.id and " +
-                "UserDebt.id = :UserDebt.id";
-
-        Map<String, Long> param = Map.of("FixedExpenditure.id", id, "FlexibleExpenditure.id", id,
-                "UserAsset.id", id, "UserDebt.id", id, "UserIncome.id", id);
+        Map<String, Long> param = Map.of("User.id", id);
 
         return template.query(sql, param, userDtoRowMapper());
     }
